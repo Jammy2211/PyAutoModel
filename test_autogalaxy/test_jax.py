e3915480787aeb8ac66566111907964b0a646b92
Richard <richardhayes777@gmail.com>
2023-03-24 14:18:39 +0000

    test flatten and unflatten

--- test_autogalaxy/test_jax.py
+++ test_autogalaxy/test_jax.py
@@ -13,21 +13,32 @@ def make_instance():
     return model.instance_from_prior_medians()
 
 
-@pytest.fixture(name="analysis")
-def make_analysis():
-    imaging = ag.Imaging(
+@pytest.fixture(name="imaging")
+def make_imaging():
+    return ag.Imaging(
         image=aa.Array2D.full(0.0, shape_native=(10, 10), pixel_scales=0.05),
         noise_map=aa.Array2D.full(1.0, shape_native=(10, 10), pixel_scales=0.05),
         psf=aa.Kernel2D.ones(shape_native=(3, 3), pixel_scales=0.05, normalize=True),
     )
 
-    mask_2d = ag.Mask2D.circular(
+
+@pytest.fixture(name="mask_2d")
+def make_mask_2d(imaging):
+    return ag.Mask2D.circular(
         shape_native=imaging.shape_native, pixel_scales=imaging.pixel_scales, radius=3.0
     )
 
+
+@pytest.fixture(name="analysis")
+def make_analysis(imaging, mask_2d):
     imaging = imaging.apply_mask(mask=mask_2d)
 
     return ag.AnalysisImaging(dataset=imaging)
+
+
+def test_flatten_mask(mask_2d):
+    flattened = jax.tree_util.tree_flatten(mask_2d)
+    jax.tree_util.tree_unflatten(flattened[1], flattened[0])
 
 
 def test_gradient(analysis, instance):

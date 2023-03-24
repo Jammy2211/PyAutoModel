3e721d2aa5c19dd4d2627bcc6a4ad504b4e65e85
Richard <richardhayes777@gmail.com>
2023-03-24 12:27:28 +0000

    extracting workshop code into tests for JAX

---
+++ test_autogalaxy/test_jax.py
@@ -0,0 +1,40 @@
+import jax
+import pytest
+
+import autoarray as aa
+import autofit as af
+import autogalaxy as ag
+
+
+@pytest.fixture(name="instance")
+def make_instance():
+    galaxy_model = af.Model(ag.Galaxy, redshift=0.5, bulge=ag.lp_operated.Sersic)
+    model = af.Collection(galaxies=af.Collection(galaxy=galaxy_model))
+    return model.instance_from_prior_medians()
+
+
+@pytest.fixture(name="analysis")
+def make_analysis():
+    imaging = ag.Imaging(
+        image=aa.Array2D.full(0.0, shape_native=(100, 100), pixel_scales=0.05),
+        noise_map=aa.Array2D.full(1.0, shape_native=(100, 100), pixel_scales=0.05),
+        psf=aa.Kernel2D.ones(shape_native=(11, 11), pixel_scales=0.05, normalize=True),
+    )
+
+    mask_2d = ag.Mask2D.circular(
+        shape_native=imaging.shape_native, pixel_scales=imaging.pixel_scales, radius=3.0
+    )
+
+    imaging = imaging.apply_mask(mask=mask_2d)
+
+    return ag.AnalysisImaging(dataset=imaging)
+
+
+def test_gradient(analysis, instance):
+    gradient = jax.grad(analysis.log_likelihood_function)
+    gradient_instance = gradient(instance)
+
+
+def test_jit(analysis, instance):
+    jitted = jax.jit(analysis.log_likelihood_function)
+    called = jitted(instance)
